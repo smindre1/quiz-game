02e810df80459decb5a981da3de9739d13be8c47
Shane Mindreau <smindre1@binghamton.edu>
2023-08-19 16:20:49 -0400

    issues with sequence variable

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -12,6 +12,7 @@ var firstChoice = document.getElementById("firstChoice");
 var secondChoice = document.getElementById("secondChoice");
 var thirdChoice = document.getElementById("thirdChoice");
 var fourthChoice = document.getElementById("fourthChoice");
+var sequence = 0;
 
 btn.addEventListener("click", quizGame);
 
@@ -20,21 +21,11 @@ function quizGame() {
   // var interval = setInterval(function () {
   //   timer.innerHTML = "Time: " + Math.floor((new Date() - start) / 1000) + " seconds";
   // }, 1000);
-  var sequence = 0;
+
   homePage.style.display = "none";
   quiz.style.display = "flex";
 
-  // for (let i = 0; i < questions.length; i) {
-  // alert("test 2");
-  problem.innerHTML = questions[sequence].question;
-  firstChoice.innerHTML = questions[sequence].choices[0];
-  firstChoice.addEventListener("click", checkAnswer);
-  secondChoice.innerHTML = questions[sequence].choices[1];
-  secondChoice.addEventListener("click", checkAnswer);
-  thirdChoice.innerHTML = questions[sequence].choices[2];
-  thirdChoice.addEventListener("click", checkAnswer);
-  fourthChoice.innerHTML = questions[sequence].choices[3];
-  fourthChoice.addEventListener("click", checkAnswer);
+  loadQuestion(sequence);
   // position = questions[sequence].choices.indexOf(questions[sequence].answer);
 
   //add a class identifier to position
@@ -71,9 +62,22 @@ var questions = [
 // either brute force it by creating a function for each question and cycling through,
 //or create an array of arrays of the questions and cycle through that.
 
+function loadQuestion(sequence) {
+  problem.innerHTML = questions[sequence].question;
+  firstChoice.innerHTML = questions[sequence].choices[0];
+  firstChoice.addEventListener("click", checkAnswer);
+  secondChoice.innerHTML = questions[sequence].choices[1];
+  secondChoice.addEventListener("click", checkAnswer);
+  thirdChoice.innerHTML = questions[sequence].choices[2];
+  thirdChoice.addEventListener("click", checkAnswer);
+  fourthChoice.innerHTML = questions[sequence].choices[3];
+  fourthChoice.addEventListener("click", checkAnswer);
+}
+
 function checkAnswer(sequence) {
   alert("seriously");
-  sequence = sequence + 1;
+  sequence++;
+  loadQuestion(sequence);
   return sequence;
 }
 
