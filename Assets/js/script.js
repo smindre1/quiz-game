d983363ea98aba101422574606479397b15631f2
Shane Mindreau <smindre1@binghamton.edu>
2023-08-22 15:23:05 -0400

    I cleaned up and commented my HTML and JS files. I also added in my own filled out README.md file with a link to my deployed program.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,29 +1,32 @@
 //Check if this is needed
 var timer = document.querySelector(".timer");
 
-var score = 0;
+//Page screen variables (HTML id's).
 var homePage = document.getElementById("home");
 var gameOverPage = document.getElementById("gameOverPage");
 var viewHighscorePage = document.getElementById("highscorePage");
 var form = document.getElementById("form");
 var initials = document.getElementById("initials");
 var submitInitials = document.getElementById("submitInitials");
-
+//Button variables (HTML id's).
 var btn = document.getElementById("startButton");
 var highscoreTab = document.getElementById("viewHighscore");
 var homepageButton1 = document.getElementById("homepageButton1");
 var homepageButton2 = document.getElementById("homepageButton2");
 var playAgain = document.getElementById("playAgainButton");
+//Question variables (HTML id's).
 var problem = document.getElementById("question");
 var firstChoice = document.getElementById("firstChoice");
 var secondChoice = document.getElementById("secondChoice");
 var thirdChoice = document.getElementById("thirdChoice");
 var fourthChoice = document.getElementById("fourthChoice");
 var finalScore = document.getElementById("finalScore");
+//Global variables, initializing values.
 var sequence = 0;
 var score = 0;
 var scoreboard = [];
-
+var timeDeducted = 0;
+//Questions stored in nested array.
 var questions = [
   {
     question: "Question 1",
@@ -51,8 +54,8 @@ var questions = [
     answer: "Three",
   },
 ];
-var timeDeducted = 0;
 
+//Buttons having event listeners added.
 btn.addEventListener("click", quizGame);
 highscoreTab.addEventListener("click", highscorePage);
 homepageButton1.addEventListener("click", homepageScreen);
@@ -60,26 +63,18 @@ homepageButton2.addEventListener("click", homepageScreen);
 playAgain.addEventListener("click", quizGame);
 
 function quizGame() {
-  // var quiz = document.getElementById("quiz");
-  // var interval = setInterval(function () {
-  //   timer.innerHTML = "Time: " + (questions.length * 15 - Math.floor((new Date() - start) / 1000) - timeDeducted) + " seconds";
-  // }, 1000);
-
-  //These are here so that if a user wants to play again the game will reset
+  //These are here so that if a user wants to play again the game will reset.
   score = 0;
   sequence = 0;
   timeDeducted = 0;
   gameOverPage.style.display = "none";
-  homePage.style.display = "flex";
+  homePage.style.display = "flex"; //Test if this line is needed*
   var start = new Date();
-  //There is and issue with trying to reset the timer, every game repeat 10 seconds is deducted from the counter.
-  //I believe it has something to do with the checkAnswers function.
   var quiz = document.getElementById("quiz");
+  //This sets up the games timer.
   var interval = setInterval(function () {
     function clock() {
       var time = questions.length * 15 - Math.floor((new Date() - start) / 1000) - timeDeducted;
-      console.log(new Date(), "new Date");
-      console.log(start, "start");
       if (time > 0) {
         timer.innerHTML = "Time: " + time + " seconds";
       }
@@ -87,7 +82,6 @@ function quizGame() {
       if (time < 0) {
         endGame();
         clearInterval(interval);
-        console.log(interval, "interval");
       }
     }
     clock();
@@ -95,6 +89,7 @@ function quizGame() {
 
   homePage.style.display = "none";
   quiz.style.display = "flex";
+  //Flips through the questions, and ends the game if there are no more questions.
   if (sequence < questions.length) {
     loadQuestion();
   } else {
@@ -105,9 +100,10 @@ function quizGame() {
 function loadQuestion() {
   if (sequence >= questions.length) {
     endGame();
-  }
+  } //Is this line needed.*
+  //This loads the question as an h2 HTML tag.
   problem.innerHTML = questions[sequence].question;
-
+  //These load the answer choice buttons and makes them clickable (event listeners).
   firstChoice.innerHTML = questions[sequence].choices[0];
   firstChoice.addEventListener("click", checkAnswer);
   secondChoice.innerHTML = questions[sequence].choices[1];
@@ -119,39 +115,45 @@ function loadQuestion() {
 }
 
 function checkAnswer(event) {
+  //Correct answer
   if (event.target.textContent == questions[sequence].answer) {
     alert("correct");
     score = score + 100;
   }
+  //Incorrect answer
   if (event.target.textContent !== questions[sequence].answer) {
-    // alert("incorrect");
     timeDeducted = timeDeducted + 10;
   }
   sequence++;
   loadQuestion(sequence);
 }
-//if time runs out, function stops and display: none currentQuestion
 
 function endGame() {
+  //Ends quiz screen and switches to game over screen.
   quiz.style.display = "none";
   timer.innerHTML = "";
   gameOverPage.style.display = "flex";
   form.style.display = "flex";
+  //Displays final score on game over page.
   finalScore.innerHTML = "Score: " + score;
+  //Resets values for next game.
   sequence = 0;
   timeDeducted = timeDeducted + 1000;
 }
 
 function highscorePage() {
+  //Ends current page screen and displays highscore page screen.
   gameOverPage.style.display = "none";
   homePage.style.display = "none";
   viewHighscorePage.style.display = "flex";
+
   var highscoreTable = document.getElementById("highscoreTable");
+  //Removes any prior list of highscores. Without this, every list generated from the user visiting the highscore page previously will stack.
   while (highscoreTable.lastChild) {
     highscoreTable.removeChild(highscoreTable.lastChild);
   }
+  //Grabs the nested array of updated highscore list from local storage and lists it in HTML.
   var storedScoreboard = JSON.parse(localStorage.getItem("user"));
-  // console.log(storedScoreboard);
   for (i = 0; i < storedScoreboard.length; i++) {
     var lineOne = highscoreTable.appendChild(document.createElement("li"));
     lineOne.innerHTML = storedScoreboard[i][0] + ": " + storedScoreboard[i][1];
@@ -159,11 +161,13 @@ function highscorePage() {
 }
 
 function homepageScreen() {
+  //Function to bring user to the homepage screen.
   gameOverPage.style.display = "none";
   viewHighscorePage.style.display = "none";
   homePage.style.display = "flex";
 }
 
+//Takes the <input> initials from the game over screen, displays it, and stores it to a nested array in local storage.
 submitInitials.addEventListener("click", function (event) {
   event.preventDefault();
   var nameInitials = initials.value.trim();
@@ -178,11 +182,4 @@ submitInitials.addEventListener("click", function (event) {
   scoreboard = storedScoreboard;
   scoreboard.push(player);
   localStorage.setItem("user", JSON.stringify(scoreboard));
-  // alert(scoreboard[1]);
 });
-
-// initials.addEventListener("submit", function () {
-//   localStorage.setItem("");
-// });
-
-//Make a new function to get the local storage and fill in the scoreboard array
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
+# Quiz-Game
+
+This is a program that creates a multiple choice timed quiz. It gives 100 points for each correct answer and deducts time from the timer for every incorrect answer. When the user either runs out of time or questions the program requests initials to tie to the attempt's score. Once provided it adds the initials and score into a nested array labeled 'scoreboard' and places it into local storage. On the top left of the screen is a text button labeled 'View Highscore' which will take the user to a new screen where the user's scores of all quiz game attempts are taken from local storage and displayed. The game can repeated infinitely, and starts from clicking the cooresponding button from either the homepage or from the game over screen.
+
+- From this project I learned how to create a timer; I learned how to properly create event listeners; how to better structure my code; and how to flip between multiple screens from a single domain.
+
+## Usage
+
+Upon clicking the button, the user will be prompted to type in a number for the length of their desired password. That will be followed by four more prompts asking what types of characters should be included in the password. The user's new password will then be written in the display box.
+
+Deployed Program (Quiz Game Website): [https://smindre1.github.io/quiz-game/](!https://smindre1.github.io/quiz-game/)
+
+## Credits
+
+All code was built by Shane Mindreau.
+
+The project idea was provided by Columbia Engineering Coding Boot Camp.
+
 I used the advice from: [resource](!https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript) to help me learn how to delete child elements of my view highscore's page, highscore table. Without it the user's highscore list would repeatedly accumulate everytime the user goes back to that screen.
+
+![Homepage Display](./Assets/images/Homepage.png)
+
+![Question Display](./Assets/images/Question-Display.png)
+
+![Game Over Display (1)](./Assets/images/Game-Over-Page-1.png)
+
+![Game Over Display (2)](./Assets/images/Game-Over-Page-2.png)
+
+![Highscore Page Display](./Assets/images/Highscore-Page.png)
+
+## License
+
+MIT License.
